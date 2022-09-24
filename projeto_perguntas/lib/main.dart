521ffdb144539dcd9d24f2aa9311b4d398811a64
FelipeCourtois <felipe.courtois@hotmail.com>
2022-09-24 18:26:24 -0300

    exibindo resultado e dividindo APP em widgets

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -2,62 +2,67 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import './questao.dart';
-import './respostas.dart';
+import './questionario.dart';
+import './resultado.dart';
 
 void main() {
-  runApp(PerguntaApp());
+  runApp(const PerguntaApp());
 }
 
 class PerguntaApp extends StatefulWidget {
+  const PerguntaApp({Key? key}) : super(key: key);
+
   @override
   State<PerguntaApp> createState() => _PerguntaAppState();
 }
 
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
+  final _perguntas = const [
+    //a lista de perguntas foi extraida
+
+    {
+      'texto': 'Qual é a sua cor favorita?',
+      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+    },
+    {
+      'texto': 'Qual é o seu instrutor favorito?',
+      'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
+    }
+  ];
 
   void _responder() {
     //metodo responder
-    setState(() {
-      _perguntaSelecionada++;
-    });
+    if (temPerguntaSelecionada) {
+      //tem pergunta???
+      setState(() {
+        _perguntaSelecionada++;
+      });
+    }
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      //variável com uma lista de perguntas
-      {
-        'texto': 'Qual é a sua cor favorita?',
-        'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
-      },
-      {
-        'texto': 'Qual é o seu animal favorito?',
-        'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
-      },
-      {
-        'texto': 'Qual é o seu instrutor favorito?',
-        'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
-      }
-    ];
-    List<String> respostas =
-        perguntas[_perguntaSelecionada].cast()['respostas'];
-
     return MaterialApp(
       home: Scaffold(
-        //Scaffold vai dar a estrutura da aplicação
         appBar: AppBar(
-          title: Text(
-              'Perguntas'), //tittle é um parâmetro nomeado que recebe um widget
-          centerTitle: true, //centraliza na appbar
+          title: const Text('Perguntas'),
         ),
-        body: Column(
-          children: [
-            Questao(perguntas[_perguntaSelecionada]['texto'] as String),
-            ...respostas.map((t) => Resposta(t, _responder)).toList(),
-          ],
-        ),
+        body: temPerguntaSelecionada
+            ? Questionario(
+                perguntas: _perguntas,
+                perguntaSelecionada: _perguntaSelecionada,
+                responder: _responder,
+              )
+            : Resultado(),
       ),
     );
   }
